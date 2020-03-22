de96d0898b9b0c18fd3f0b362afad3500edeff47
gonzalezenios <gonzalezeniood@gmail.com>
2020-03-22 04:01:58 -0300

    good practices

--- aprendiendoreact/src/components/MiComponente.js
+++ aprendiendoreact/src/components/MiComponente.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class MiComponente extends React.Component {
+class MiComponente extends Component {
 
     render() {
+
+        let receta = {
+            nombre: 'Pizza',
+            ingredientes: ['Tomate', 'Queso', 'Jamon cocido'],
+            calorias: 400
+        }
+
         return (
-        <h1>Hola, soy el componente llamada: MiComponente</h1>
+            <div className="mi-componente">
+                <h1>{'Receta: ' +  receta.nombre}</h1>
+                <h2>{'Calorias: ' + receta.calorias}</h2>
+                <ol>
+                {
+                    receta.ingredientes.map((ingrediente, i) => {
+                        console.log(ingrediente)
+                        return (
+                            <li key={i}>
+                                {ingrediente}
+                            </li>
+                        )
+                    })
+                }
+                </ol>
+            </div>
+        
         )
     }
 }
